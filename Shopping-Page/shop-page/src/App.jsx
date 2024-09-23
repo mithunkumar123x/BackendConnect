5b75188b72252df000e3651bf426187f6a885855
mithunkumar123x <mithunkumar123x@gmail.com>
2024-09-23 14:29:13 +0530

    fixing cart problem

--- Shopping-Page/shop-page/src/App.jsx
+++ Shopping-Page/shop-page/src/App.jsx
@@ -1,82 +1,51 @@
-import Layout from "./components/Layout/Layout";
-import Cart from "./components/Cart/Cart";
-import Products from "./components/Shop/Product";
-import './App.css';
-import { useDispatch, useSelector } from "react-redux";
-import { uiActions } from "./Store/UI-Slice";
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Product';
 import Notification from './components/UI/Notification';
+import { fetchCartData, sendCartData } from './Store/cart-actions';
 
 let isInitial = true;
 
 function App() {
-    
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
-  useEffect(() => {
-    const sendCartData = async() => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json',
-        {
-          method: 'PUT' ,
-          body: JSON.stringify(cart),
-        }
-      );
+useEffect(() => {
+  dispatch(fetchCartData());
+}, [dispatch]);
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-     
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-      
+
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+     }
+    }, [cart, dispatch]) ;
+    
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      )
-    })
-  }, [cart , dispatch]);
+
 
   return (
     <Fragment>
       {notification && (
-        <Notification 
-          status = {notification.status}
-          title= {notification.title}
-          message = {notification.message}
-
-          />
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
       )}
-   
-    <Layout>
-     {showCart &&  <Cart />}
-      <Products />
-    </Layout>
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
     </Fragment>
   );
 }
--- Shopping-Page/shop-page/src/Store/cart-Slice.js
+++ Shopping-Page/shop-page/src/Store/cart-Slice.js
@@ -1,10 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
     replaceCart(state, action) {
@@ -15,6 +17,7 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -32,14 +35,18 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
+
+
 
 export const cartActions = cartSlice.actions;
 
